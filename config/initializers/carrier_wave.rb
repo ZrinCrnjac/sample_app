df2e947a4e9dac95aa6826f16a83591c190ea3a4
Zrin Crnjac <zrin.crnjac@gmail.com>
2019-06-22 21:51:58 +0200

    Server still not fixed

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,6 +2,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
+      :region => ENV['S3_REGION'],
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['AKIA57ZUMUAGVWATTB4Z'],
       :aws_secret_access_key => ENV['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']
